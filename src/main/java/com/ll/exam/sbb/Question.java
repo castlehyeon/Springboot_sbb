6e6db74bbe594cf59a810a9b32a81df0e5707e1f
castlehyeon <qnftktls118@naver.com>
2022-08-10 16:50:25 +0900

    Repository 생성

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -10,17 +10,21 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
+//컨트롤러에 @Controller 애너테이션을 적용하는 것과 마찬가지로 엔티티는 @Entity 애너테이션을 적용
 public class Question {
     @Id
+    //고유 번호 id 속성에 적용한 @Id 애너테이션은 id 속성을 기본 키로 지정
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //@GeneratedValue 애너테이션을 적용하면 데이터를 저장할 때 해당 속성에 값을 따로 세팅하지 않아도 1씩 자동으로 증가하여 저장
     private Integer id;
 
     @Column(length = 200)
     private String subject;
 
     @Column(columnDefinition = "TEXT")
+    //columnDefinition = "TEXT"은 "내용"처럼 글자 수를 제한할 수 없는 경
     private String content;
-
+    //엔티티의 속성은 @Column 애너테이션을 사용하지 않더라도 테이블 컬럼으로 인식한다. 테이블 컬럼으로 인식하고 싶지 않은 경우에만 @Transient 애너테이션을 사용
     private LocalDateTime createDate;
 
     @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
