31c66494837e29c8e38b5b60ddf2d1a6e4333048
castlehyeon <qnftktls118@naver.com>
2022-08-10 14:38:15 +0900

    jpa로 question answer 생성

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Getter
 @Setter
@@ -21,5 +22,13 @@ public class Question {
     private String content;
 
     private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    //mappedBy는 참조 엔티티의 속성명을 의미
+    //Answer 엔티티에서 Question 엔티티를 참조한 속성명 question을 mappedBy에 전달
+    //질문을 삭제하면 그에 달린 답변들도 모두 함께 삭제하기 위해서 @OneToMany의 속성으로 cascade = CascadeType.REMOVE
+    private List<Answer> answerList;
+    //질문과 답변은 1:N의 관계다.
+    //질문객체에서 답을 참조하기 위해
 }
 
