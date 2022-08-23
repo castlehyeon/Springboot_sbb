fc183b7a020db0e765a50d8cf98a9cb055ea8f1a
castlehyeon <qnftktls118@naver.com>
2022-08-24 08:35:37 +0900

    0824restart

---
+++ src/main/java/com/ll/exam/sbb/Member.java
@@ -0,0 +1,14 @@
+package com.ll.exam.sbb;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
